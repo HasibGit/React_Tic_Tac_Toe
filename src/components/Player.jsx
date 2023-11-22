876fb74b6c1ff9fda41472b7d9f5a86a2e2d79ff
HasibGit <hasibullah04@gmail.com>
2023-11-23 00:46:37 +0600

    added two way data binding

---
+++ src/components/Player.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [playerName, setPlayerName] = useState(name);
+  const [isEditing, setIsEditing] = useState(false);
+
+  return (
+    <li>
+      <span className="player">
+        {!isEditing && <span className="player-name">{playerName}</span>}
+        {isEditing && (
+          <input
+            type="text"
+            value={playerName}
+            required
+            onChange={() => setPlayerName(event.target.value)}
+          />
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+
+      {!isEditing && <button onClick={() => setIsEditing(true)}>Edit</button>}
+      {isEditing && <button onClick={() => setIsEditing(false)}>Save</button>}
+    </li>
+  );
+}
