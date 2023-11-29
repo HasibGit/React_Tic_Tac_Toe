b98ed1ba946c4ddcb59eafd4b9c35a8245711334
Hasib Ullah <hasibullah04@gmail.com>
2023-11-29 18:31:27 +0600

    Transferred states from game board to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,64 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Logs from "./components/Logs";
 import GameOver from "./components/GameOver";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { WINNING_COMBINATIONS } from "../winning_combinations";
+
+const initialBoardState = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function App() {
+  const [gameBoard, setGameBoard] = useState(initialBoardState);
   const [currentActivePlayer, setCurrentActivePlayer] = useState("X");
   const [player1Name, setPlayer1Name] = useState("Player 1");
   const [player2Name, setPlayer2Name] = useState("Player 2");
   const [turns, setTurns] = useState([]);
   const [winnerSymbol, setWinnerSymbol] = useState("");
+
+  useEffect(() => {
+    // Check for a winner after the gameBoard state has been updated
+    for (let combination of WINNING_COMBINATIONS) {
+      const firstCell = gameBoard[combination[0].row][combination[0].column];
+      const secondCell = gameBoard[combination[1].row][combination[1].column];
+      const thirdCell = gameBoard[combination[2].row][combination[2].column];
+
+      if (firstCell && firstCell === secondCell && secondCell === thirdCell) {
+        setWinnerSymbol(firstCell);
+        break;
+      }
+    }
+  }, [gameBoard, setWinnerSymbol]);
+
+  const handleSelectSquare = (rowIndex, colIndex) => {
+    setGameBoard((prevGameBoard) => {
+      const updatedGameBoard = [...prevGameBoard.map((row) => [...row])];
+      updatedGameBoard[rowIndex][colIndex] = currentActivePlayer;
+
+      return updatedGameBoard;
+    });
+
+    setTurns((prevTurns) => {
+      return [
+        {
+          cell: { row: rowIndex, col: colIndex },
+          symbol:
+            prevTurns.length > 0
+              ? prevTurns[0].symbol === "X"
+                ? "O"
+                : "X"
+              : "X",
+        },
+        ...prevTurns,
+      ];
+    });
+
+    setCurrentActivePlayer((prevPlayer) => {
+      return prevPlayer === "X" ? "O" : "X";
+    });
+  };
 
   return (
     <main>
@@ -36,10 +86,8 @@ function App() {
         )}
 
         <GameBoard
-          currentPlayerSymbol={currentActivePlayer}
-          switchPlayer={setCurrentActivePlayer}
-          setTurns={setTurns}
-          setWinnerSymbol={setWinnerSymbol}
+          gameBoard={gameBoard}
+          onSquareClick={handleSelectSquare}
         ></GameBoard>
       </div>
 
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,62 +1,4 @@
-import { useState, useEffect } from "react";
-import { WINNING_COMBINATIONS } from "../../winning_combinations";
-
-const initialBoardState = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({
-  switchPlayer,
-  currentPlayerSymbol,
-  setTurns,
-  setWinnerSymbol,
-}) {
-  const [gameBoard, setGameBoard] = useState(initialBoardState);
-
-  useEffect(() => {
-    // Check for a winner after the gameBoard state has been updated
-    for (let combination of WINNING_COMBINATIONS) {
-      const firstCell = gameBoard[combination[0].row][combination[0].column];
-      const secondCell = gameBoard[combination[1].row][combination[1].column];
-      const thirdCell = gameBoard[combination[2].row][combination[2].column];
-
-      if (firstCell && firstCell === secondCell && secondCell === thirdCell) {
-        setWinnerSymbol(firstCell);
-        break;
-      }
-    }
-  }, [gameBoard, setWinnerSymbol]);
-
-  const handleSelectSquare = (rowIndex, colIndex) => {
-    setGameBoard((prevGameBoard) => {
-      const updatedGameBoard = [...prevGameBoard.map((row) => [...row])];
-      updatedGameBoard[rowIndex][colIndex] = currentPlayerSymbol;
-
-      return updatedGameBoard;
-    });
-
-    setTurns((prevTurns) => {
-      return [
-        {
-          cell: { row: rowIndex, col: colIndex },
-          symbol:
-            prevTurns.length > 0
-              ? prevTurns[0].symbol === "X"
-                ? "O"
-                : "X"
-              : "X",
-        },
-        ...prevTurns,
-      ];
-    });
-
-    switchPlayer((prevPlayer) => {
-      return prevPlayer === "X" ? "O" : "X";
-    });
-  };
-
+export default function GameBoard({ gameBoard, onSquareClick }) {
   return (
     <ol id="game-board">
       {gameBoard.map((row, rowIndex) => (
@@ -68,7 +10,7 @@ export default function GameBoard({
                   disabled={
                     gameBoard[rowIndex][colIndex] != null ? true : false
                   }
-                  onClick={() => handleSelectSquare(rowIndex, colIndex)}
+                  onClick={() => onSquareClick(rowIndex, colIndex)}
                 >
                   {playerValue}
                 </button>
